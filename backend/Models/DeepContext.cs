4a4112075707ca6c048f80f714373192b050dcdd
andrey-danilov <andreydanilov.com@gmail.com>
2019-01-01 23:06:31 +0200

    Add DbContext

---
+++ backend/Models/DeepContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class DeepContext : DbContext
+    {
+        //public DbSet<Phone> Phones { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+
+        public DeepContext(DbContextOptions<DeepContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+    
+}
